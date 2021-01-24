563e82045920a8e4cff310b85e029b64231017c1
MAURO <mfconsolani@outlook.com>
2021-01-24 23:36:54 +0100

    modifications en ejercicio carpeta 5 y 6

--- Clase6/nodeFileHandling/fileIOTest.js
+++ Clase6/nodeFileHandling/fileIOTest.js
@@ -3,17 +3,28 @@ const { Archivo } = require('./handlerClassIO');
 const testOne = new Archivo('./archivodeprueba.txt')
 
 // Leyendo archivo inexistente
-setTimeout(() => testOne.readFile(), 1000)
+setTimeout(() => {
+    console.log('Leyendo productos disponibles')
+    testOne.readFile()
+}, 1000)
 
 // Agregando dos productos al archivo
 
-setTimeout(() => testOne.saveFile('Iphone 6', 700, 11), 2000);
+setTimeout(() => {
+    console.log('Guardando 1er producto')
+    testOne.saveFile('Iphone 6', 700, 'url/a/Iphone6Modelo-2020-u-otro')
+}, 2000);
 
-setTimeout(() => testOne.saveFile('Lenovo Thinkpad T14', 1400, 121), 3000);
+setTimeout(() => {
+    console.log('Guardando 2do producto')
+    testOne.saveFile('Lenovo Thinkpad T14', 1400, 'url/a/LenovoThinkpadT14')
+}, 3000);
 
 // Leyendo archivo con 2 items
-setTimeout(() => testOne.readFile(), 6000)
+setTimeout(() => {
+    console.log('Leyendo productos disponibles')
+    testOne.readFile()
+}, 6000)
 
 // Borrando archivo 
-
 setTimeout(() => testOne.deleteFile(), 10000);
