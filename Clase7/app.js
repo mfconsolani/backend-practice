51cdcb9801c6c786a3fdcf9b5869ecdf10bfda59
MAURO <mfconsolani@outlook.com>
2021-01-31 15:37:19 +0100

    added .txt as part of exercise. Also, finished app.js. It takes a .txt files and make http get request that deal with the information into the .txt file. Next step is adding TypeScript

--- Clase7/app.js
+++ Clase7/app.js
@@ -1,16 +1,59 @@
 const express = require('express');
 
+const fs = require('fs');
+const { parse } = require('path');
+
 const app = express();
 
 app.use(express.json());
 
-app.use(express.urlencoded({extendend: true})))
+app.use(express.urlencoded({extendend: true}))
 
-app.get('/items', (res, req) => {
+let itemCount = 0
 
+let itemsCount = 0
+
+async function fileParser(fileToParse){
+    const file = await fs.promises.readFile(fileToParse, 'utf-8')
     
+    const parsedFile = await JSON.parse(file);
+    
+    return parsedFile
+}
+
+app.get('/items', async (req, res) => {
+
+    const productos = await fileParser('./productos.txt')
+
+    const listadoProd = {
+        items: productos, 
+        cantidad: productos.length
+    }
+
+    itemsCount++
+
+    res.status(200).send(listadoProd);
 })
 
+app.get('/item-random', async (req, res) => {
+
+    const productos = await fileParser('./productos.txt')
+
+    const randNum = Math.ceil(Math.random() * (productos.length));
+
+    const randItem = productos.filter(item => item.id === randNum)
+
+    itemCount++
+
+    res.status(200).json(randItem)
+});
+
+
+app.get('/visitas', (req, res) => {
+
+    res.send({visitas: itemsCount, visita: itemCount})
+
+})
 
 const PORT = 8080;
 
